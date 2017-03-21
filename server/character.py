a0c81c62ffda422fe5794bcd9a6ce929fd92c8e0
TheDarrenJoseph <thedarrenjoseph@gmail.com>
2017-03-21 21:46:34 +0000

    Moved files into a module

--- server/character.py
+++
@@ -1,9 +0,0 @@
-import database, player
-from pony.orm import *
-
-class Character(database.DatabaseHandler._database.Entity):
-    charname = PrimaryKey(str)
-    #Positions are stored relative to the map
-    posX = Required(int)
-    posY = Required(int)
-    player=Optional('Player')
--- server/mapTile.py
+++
@@ -1,4 +0,0 @@
-class MapTile():
-    def __init__(self):
-        self.tileType = 0
-        self.traversible = True
