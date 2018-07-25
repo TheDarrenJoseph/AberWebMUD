264c0732c173097ea6386ba3443958c0a78c5d83
TheDarrenJoseph <thedarrenjoseph@gmail.com>
2018-07-25 18:48:16 +0100

    Dice test fixes, fixed gitignore rule for tests.js

--- server/tests/test_dice.py
+++ server/tests/test_dice.py
@@ -3,14 +3,17 @@ import sys
 import os.path
 from pyfiles import dice
 
+min_dice_face_val = 2
+
 max_dice_face_val = 100
 max_dice_count = 10
+max_max_range = max_dice_count*max_dice_count
 
 class diceRollGood(unittest.TestCase):
 
     def check_value(self, dice_value, face_count):
-        if dice_value > face_count or dice_value < 1:
-            self.fail('Bad dice value: '+str(dice_value))
+        self.assertLessEqual(dice_value, face_count)
+        self.assertGreaterEqual(dice_value, 1)
 
     #Rolls each supported die type once and checks the range of results
     def test_dice_roll_single(self):
@@ -21,17 +24,21 @@ class diceRollGood(unittest.TestCase):
     #Rolls a 2 sided die 10 times, checking the range
     def test_dice_roll_min_face(self):
         for n in range(1, 10):
-            dice_value = dice.rollDice(1, 2)
-            self.check_value(1, 2)
+            dice_value = dice.rollDice(1, min_dice_face_val)
+            self.check_value(dice_value, min_dice_face_val)
 
     #Rolls the max supported die count many times and checks the range of results
     def test_dice_roll_many_max(self):
         die_face = 6
-        max_range = 6*6
+        max_range = max_dice_count*6
+        #Roll as many times as we have die_faces
         for n in range(1, die_face):
             dice_value = dice.rollDice(max_dice_count, die_face)
-            if dice_value > max_range or dice_value < 6:
-                self.fail('Bad dice value: '+str(dice_value))
+
+            # Lowest possible
+            self.assertGreaterEqual(dice_value, max_dice_count)
+            # Total should be less than or equal to the max value
+            self.assertLessEqual(dice_value, max_range)
 
     #Rolls the min supported die count many times and checks the range of results
     def test_dice_roll_many_min(self):
@@ -41,16 +48,17 @@ class diceRollGood(unittest.TestCase):
             self.check_value(dice_value, max_roll_result)
 
     #Test rolling the maximum number of dies at once
-    def test_dice_roll_max(self):
+    def test_dice_roll_max_count(self):
         for n in range(1, 2):
+            # Flip max_dice_count coins
             dice_value = dice.rollDice(max_dice_count, 2)
-            self.check_value(dice_value, 20)
+            self.check_value(dice_value, max_dice_count*2)
 
     #Test rolling the maximum die face count multiple times (range check)
-    def test_dice_roll_max(self):
+    def test_dice_roll_max_faces(self):
         for n in range(1, 2):
             dice_value = dice.rollDice(1, max_dice_face_val)
-            self.check_value(dice_value, 20)
+            self.check_value(dice_value, max_dice_face_val)
 
 class diceRollBad(unittest.TestCase):
     exception_response = None
