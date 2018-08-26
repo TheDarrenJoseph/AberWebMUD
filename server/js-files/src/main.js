bff3caac46fd655a5389def808a187f52817ae38
TheDarrenJoseph <thedarrenjoseph@gmail.com>
2018-08-26 20:13:44 +0100

    Cleaned/Improved webpack config; w/ added aliases for libs and src, moved libs out of src, updated the main JS file to fix jQuery import and get ready for general imports

--- server/js-files/src/main.js
+++ server/js-files/src/main.js
@@ -1,23 +1,24 @@
-//Can't fix this yet
-import {$, jQuery} from './libs/jquery-3.1.1.js';
+import $ from 'libs/jquery-3.1.1.js';
+import { SocketHandler } from 'src/socket/handle-socket';
+//	import { setupPageUI } from './ui/pixi/setup-ui';
 
-import { connectSocket, setupChat, setStatusUpdateCallbacks } from './socket/handle-socket';
-import { setupPageUI } from './ui/pixi/setup-ui';
-
+console.log('=== AberWebMUD Web Client ===')
 
 //  Sets up client elements, hooks up callbacks to enable event-driven reponses, then asks the server for a map update
 function performSetup () {
-	console.log ("Starting client..");
-	
-	//connectSocket();
-	//setupPageUI();
-	//setupChat();
-	//setStatusUpdateCallbacks();
+	console.log('Starting client..');
 
-	//socket.emit('map-data-request');
+	//Get the general UI ready
+	//setupPageUI();
+
+	var connected = SocketHandler.connectSocket();
+	if (connected) {
+		//setupChat();
+		//setStatusUpdateCallbacks();
+		//socket.emit('map-data-request');
+	}
 }
 
 console.log("Awaiting page load..");
-
 //	Initialises the client setup once the HTML page loads
-$(document).on('ready', performSetup);
+$(document).ready(performSetup);
--- server/js-files/webpack.config.js
+++ server/js-files/webpack.config.js
@@ -1,28 +1,37 @@
 const path = require('path');
-var $ = require('./src/libs/jquery-3.1.1.js');
+
+const TARGET = 'web';
+const MODE = 'development';
+const OUT_PATH = path.resolve(__dirname, '../static/');
+const ALIASES = {
+	libs: path.resolve(__dirname, 'libs/'),
+	src: path.resolve(__dirname, 'src/')
+}
 
 const serverConfig = {
-  target: 'web',	
-  mode: "production", // "production" | "development" | "none" 
+  target: TARGET,
+  mode: MODE, // "production" | "development" | "none"
   entry: './src/main.js',
+	resolve: { alias: ALIASES },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, '../static/')
+    path: OUT_PATH
   }
 }
 
 //In case we need a seperate tests bundling
 const testsConfig = {
-  target: 'web',	
-  mode: "production", // "production" | "development" | "none" 
+  target: TARGET,
+  mode: MODE, // "production" | "development" | "none"
   entry: './test/main.js',
+	resolve: { alias: ALIASES },
   output: {
     filename: 'tests.js',
-    path: path.resolve(__dirname, '../static/')
+    path: OUT_PATH
   }
 }
 
+//Node.js exports, intereted by Webpack
 module.exports = [
 serverConfig, testsConfig
 ];
-
