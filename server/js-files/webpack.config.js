e24b62c0836a33e29d5dcf09a34877c7fc09d00e
TheDarrenJoseph <thedarrenjoseph@gmail.com>
2018-10-21 22:39:42 +0100

    Added Webpack cheap-module-source-map for development debugging. Removed unneeded old Webpack config.

--- server/js-files/webpack.config.js
+++ server/js-files/webpack.config.js
@@ -1,37 +1,42 @@
 const path = require('path');
-
 const TARGET = 'web';
 const MODE = 'development';
 const OUT_PATH = path.resolve(__dirname, '../static/');
 const ALIASES = {
 	libs: path.resolve(__dirname, 'libs/'),
 	src: path.resolve(__dirname, 'src/')
-}
+};
 
 const serverConfig = {
-  target: TARGET,
-  mode: MODE, // "production" | "development" | "none"
-  entry: './src/main.js',
+	target: TARGET,
+	mode: MODE, // "production" | "development" | "none"
+	entry: './src/main.js',
 	resolve: { alias: ALIASES },
-  output: {
-    filename: 'main.js',
-    path: OUT_PATH
-  }
+
+	output: {
+		filename: 'main.js',
+		path: OUT_PATH
+	}
+};
+
+// Enable development source mapping
+if (MODE === 'development') {
+	serverConfig.devtool = 'cheap-module-source-map';
 }
 
 //	In case we need a separate tests bundling
 const testsConfig = {
-  target: TARGET,
-  mode: MODE, // "production" | "development" | "none"
-  entry: './test/main.js',
+	target: TARGET,
+	mode: MODE, // "production" | "development" | "none"
+	entry: './test/main.js',
 	resolve: { alias: ALIASES },
-  output: {
-    filename: 'tests.js',
-    path: OUT_PATH
-  }
-}
+	output: {
+		filename: 'tests.js',
+		path: OUT_PATH
+	}
+};
 
 //	Node.js exports, intereted by Webpack
 module.exports = [
-serverConfig, testsConfig
+	serverConfig, testsConfig
 ];
