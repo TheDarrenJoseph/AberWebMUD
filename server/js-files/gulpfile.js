22c2e6a8b9963199983f1b93ff8d0a9c5605f8e8
Darren Joseph <thedarrenjoseph@gmail.com>
2019-07-19 22:27:00 +0100

    Adding gulp.js code to automatically construct our testing main.js

--- server/js-files/gulpfile.js
+++ server/js-files/gulpfile.js
@@ -13,7 +13,10 @@ const commonjs = require ('rollup-plugin-commonjs');
 
 const del = require('delete');
 const path = require('path');
+const fs = require('fs');
 
+// For gulp readable streams
+const through = require('through2')
 
 const MAINFILE_IN_URI = 'src/main.js';
 const TESTFILE_IN_URI = 'test/main.js';
@@ -29,6 +32,80 @@ const paths = {
   libs : 	 path.resolve(__dirname, 'libs/'), 
   test: 	 path.resolve(__dirname, 'test/')
 };
+
+const EXCLUDED_TESTFILES = [TESTFILE_IN_URI, 'MapModelBuilder.js']
+const EXCLUDED_TESTPATHS = ['utils/']
+
+/**
+function generateImport(vinylFile) {
+	//console.log(JSON.stringify(chunk))
+	let filePath = vinylFile.path;
+	let relativePath = filePath.match("test/.*")[0]
+
+	return 'import \''+relativePath+'\'';
+}
+**/
+
+function generateImportLine(vinylFile, enc, cb) {
+	//console.log(JSON.stringify(chunk))
+	let filePath = vinylFile.path;
+	let relativePath = filePath.match("test/.*")[0]
+
+	let excluded = false;
+	EXCLUDED_TESTPATHS.forEach( excludedPath => {
+		if (relativePath.includes(excludedPath)) {
+			console.log('Excluding: ' + relativePath + ' from import declarations, matches exclusion: ' + excludedPath);
+			excluded = true;
+			return;
+		}
+	});
+	
+	if (!excluded) {
+		EXCLUDED_TESTFILES.forEach( excludedPath => {
+			if (relativePath.endsWith(excludedPath)) {
+				console.log('Excluding: ' + excludedPath + ' from import declarations.. ');
+				excluded = true;
+				return;
+			}
+		});
+	}
+
+	
+	if (excluded) {
+		cb();
+	} else { 
+		vinylFile.importLine = 'import \''+relativePath+'\'';
+		cb(null, vinylFile);
+	} 
+}
+
+function genTestMainfile(cb) {
+	console.log('=== Generating the test main.js file ===')
+	let testFilesPath = paths['test']+"/**/*.js"
+	let outputPath = path.resolve(TESTFILE_IN_URI);
+	console.log(testFilesPath);
+	console.log(outputPath);
+	let fileStream = src(testFilesPath)
+	let imports = []
+	/**
+	fileStream.on('data', (vinylFile) => { 
+		let imp = generateImport(vinylFile) 
+		if (imp !== undefined) imports.push(imp);
+	})
+	//fileStream.on('readable', () => { generateImports(fileStream.read())})
+	fileStream.on('end',() =>{
+		console.log(imports);
+		cb()
+	})
+	**/
+
+	fileStream.pipe(through.obj(generateImportLine))
+		.pipe(through.obj((file, enc, cb) => {
+			fs.appendFile(TESTFILE_IN_URI, file.importLine + '\n' , cb );
+		}))
+
+	return fileStream;
+}
 
 function rollupBundleModule(inUri, outUri, cb) {
 	console.log('rollup bundling: ' + inUri);
@@ -67,11 +144,18 @@ function promiseMainModule(cb) {
 function promiseTestModule(cb) {
 	return rollupBundleModule(TESTFILE_IN_URI, TESTFILE_OUT_URI, cb);
 }
+	
 
-function clean (cb) {
+function cleanInputFiles (cb) {
+	console.log('Cleaning up test mainfile..');
+	del([ TESTFILE_IN_URI ], {force: true}, cb);
+}
+
+
+function cleanOutputFiles (cb) {
 	console.log('Cleaning up static JS output files..');
 	// Just delete any output files we find
-	del([MAINFILE_OUT_URI, TESTFILE_OUT_URI, SOURCEMAP_WILDCARD_URI], {force: true}, cb);
+	del([MAINFILE_OUT_URI, TESTFILE_OUT_URI, SOURCEMAP_WILDCARD_URI ], {force: true}, cb);
 }
 
 // Bundle / Minify
@@ -95,17 +179,19 @@ function build (cb) {
 
 // Clean, rebuild, and watch for changes
 function buildAndWatchFiles(cb) {
-	clean();
+	cleanInputFiles()
+	cleanOutputFiles();
 	build();
 	console.log('Watching for changes..');
-	watch("src/**/*.js", series(clean, buildMain));
-	watch("test/**/*.js", series(clean, buildTest));
+	watch("src/**/*.js", series(cleanInputFiles, cleanOutputFiles, buildMain));
+	watch("test/**/*.js", series(cleanInputFiles, cleanOutputFiles, genTestMainFile, buildTest));
 }
 
 console.log('=== AberWebMUD JS Client ===')
-exports.default = series(clean, build);
+exports.default = series(cleanInputFiles, cleanOutputFiles, genTestMainfile, build);
 exports.watch = buildAndWatchFiles;
 exports.build = build;
 exports.buildTest = buildTest;
-exports.clean = clean;
+exports.buildTestMain = genTestMainfile;
+exports.clean = series(cleanInputFiles, cleanOutputFiles);
 
