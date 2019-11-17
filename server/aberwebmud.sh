f0dd2f230d15ba257cec8f7d6bad8db08f626579
Darren Joseph <thedarrenjoseph@gmail.com>
2019-11-17 18:44:47 +0000

    Adding server status function for launcher script

--- server/aberwebmud.sh
+++ server/aberwebmud.sh
@@ -3,6 +3,23 @@
 
 PIDFILE="/tmp/aberwebmud.pid"
 
+RUNNING=false
+
+function server_status() {
+	if [ -s $PIDFILE ] 
+	then
+		PID=$(<"$PIDFILE")
+		PID_COUNT=$(ps --no-heading -p "$PID" | wc -l)
+		echo "PID Count: $PID_COUNT"
+		RUNNING=$(( "$PID_COUNT" >= 0 ))
+		test "$RUNNING" && echo "Running as PID: $PID" || echo "Not running.."
+
+	else
+		echo "No pidfile at $PIDFILE, server not running?"
+	fi
+	
+}
+
 case $1 in
 	start)
 		if [ -s $PIDFILE ] 
@@ -18,17 +35,12 @@ case $1 in
 		if [ -s $PIDFILE ] 
 		then
 			cat "$PIDFILE" | xargs kill --verbose
+			rm "$PIDFILE"
 		else
 			echo "No pidfile at $PIDFILE, server not running?"
 		fi
 		;;
 	status)
-		if [ -s $PIDFILE ] 
-		then
-			PID=$(<"$PIDFILE")
-			echo "Server running with PID: $PID"
-		else
-			echo "No pidfile at $PIDFILE, server not running?"
-		fi
+		server_status
 		;;
 esac
